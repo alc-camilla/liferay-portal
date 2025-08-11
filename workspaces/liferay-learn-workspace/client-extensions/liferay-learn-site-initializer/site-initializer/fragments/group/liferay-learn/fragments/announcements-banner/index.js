cc4ee2bdf2d473f0ad538befc9d5d11aea41d889
Camilla Almeida <camilla.almeida@liferay.com>
2025-08-11 14:18:25 -0300

    [Updated]LRSD-9242 - Add an announcement banner to Learn pages

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/announcements-banner/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/announcements-banner/index.js
@@ -1,39 +1,32 @@
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
-const publicSiteNavigationContainer = document.querySelector(
-	'.public-site-navigation-container'
-);
+const publicSiteNavigationContainer = document.querySelector('.public-site-navigation-container');
 
 const announcementsBanner = document.querySelector('.announcements-banner');
 const BANNER_CLOSED_SESSION_KEY = 'bannerWasClosed';
+const isInEditMode = document.body.classList.contains('has-edit-mode-menu');
+
+if (isInEditMode) {
+	announcementsBanner.style.display = 'flex';
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
+
 	const userClosedBanner = sessionStorage.getItem(BANNER_CLOSED_SESSION_KEY);
+
 
 	if (userClosedBanner === 'true') {
 		announcementsBanner.style.display = 'none';
-		publicSiteNavigationContainer.classList.remove(
-			'navigation-margin-true'
-		);
-
+		publicSiteNavigationContainer.classList.remove('navigation-margin-true');
 		return;
 	}
 
 	if (announcementsBanner) {
 		if (themeDisplay.isSignedIn()) {
 			announcementsBanner.style.top = '56px';
-			publicSiteNavigationContainer.classList.add(
-				'navigation-margin-true'
-			);
-		}
-		else {
+			publicSiteNavigationContainer.classList.add('navigation-margin-true');
+		} else {
 			announcementsBanner.style.top = '0px';
-			publicSiteNavigationContainer.classList.add(
-				'navigation-margin-true'
-			);
+			publicSiteNavigationContainer.classList.add('navigation-margin-true');
 		}
 	}
 
@@ -41,10 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	document.querySelector('.icon-x').addEventListener('click', () => {
 		document.querySelector('.announcements-banner').style.display = 'none';
-		publicSiteNavigationContainer.classList.remove(
-			'navigation-margin-true'
-		);
+		publicSiteNavigationContainer.classList.remove('navigation-margin-true')
 
 		sessionStorage.setItem(BANNER_CLOSED_SESSION_KEY, 'true');
+
+		const navigationContainer = document.querySelector('.navigation-container');
+
 	});
+
 });
